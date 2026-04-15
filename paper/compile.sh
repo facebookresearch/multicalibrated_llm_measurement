4c12faaf56965c23ba25ee2d401ad230e6731595
Fridolin Linder <flinder@meta.com>
2026-04-15 10:25:31 +0200

    Add SI appendix compilation to build script

--- paper/compile.sh
+++ paper/compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 pandoc paper.md -o paper.pdf --pdf-engine=pdflatex --citeproc --bibliography=references.bib
+pandoc si_appendix.md -o si_appendix.pdf --pdf-engine=pdflatex --citeproc --bibliography=references.bib
